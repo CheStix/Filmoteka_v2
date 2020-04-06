4bc79231b5fd709bd40d69e9b823c0de25b17a21
Che.El <che.eldar@gmail.com>
2020-04-06 23:43:40 +0300

    review post, movie detail changes

--- filmoteka/movies/serializers.py
+++ filmoteka/movies/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Movie
+from .models import Movie, Review
 
 
 class MovieListSerializer(serializers.ModelSerializer):
@@ -10,13 +10,29 @@ class MovieListSerializer(serializers.ModelSerializer):
         fields = ('title', 'tagline', 'category')
 
 
+class ReviewCreateSerializer(serializers.ModelSerializer):
+    """creating a review serializer"""
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    """review serializer"""
+    class Meta:
+        model = Review
+        fields = ('name', 'text', 'parent')
+
+
 class MovieDetailSerializer(serializers.ModelSerializer):
     """Movie detail"""
     category = serializers.SlugRelatedField(slug_field='name', read_only=True)
     directors = serializers.SlugRelatedField(slug_field='name', read_only=True, many=True)
     actors = serializers.SlugRelatedField(slug_field='name', read_only=True, many=True)
     genres = serializers.SlugRelatedField(slug_field='name', read_only=True, many=True)
+    reviews = ReviewSerializer(many=True)
 
     class Meta:
         model = Movie
         exclude = ('draft',)
+
