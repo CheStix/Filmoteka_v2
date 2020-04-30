e68bb24db74191f757168811837a690be37575d9
Che.El <che.eldar@gmail.com>
2020-04-30 17:24:00 +0300

    install djoser for registration, authorization install simple_jwt for jwt authentication

--- README.md
+++ README.md
@@ -6,8 +6,10 @@ Description, Actors, Directors, Reviews, Ratings, etc.
 1. List of movies
 2. Movie detail
 3. Movie reviews and responses to reviews
+4. Rating to movie, average rating
+5. List and full description of actors and directors
+6. Filter movies by year and genre (/?year_min=2019&year_max=2020&genres=Комедия)
 
-//Filter by genre and year
 
 //Last added films
 
--- filmoteka/filmoteka/settings.py
+++ filmoteka/filmoteka/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from datetime import timedelta
+
 from . import prod
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -39,8 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'ckeditor',
     'ckeditor_uploader',
+    'djoser',
     'django_filters',
 
     'movies.apps.MoviesConfig',
@@ -204,8 +208,55 @@ CKEDITOR_CONFIGS = {
     }
 }
 
+# smtp
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'email_adress'
+EMAIL_HOST_PASSWORD = 'password'
+EMAIL_PORT = 587
+
+# djoser conf
+DJOSER = {
+    'PASSWORD_RESET_CONFIRM_URL': '#/password/reset/confirm/{uid}/{token}',
+    'USERNAME_RESET_CONFIRM_URL': '#/username/reset/confirm/{uid}/{token}',
+    'ACTIVATION_URL': '#/activate/{uid}/{token}',
+    'SEND_ACTIVATION_EMAIL': False,
+    'SERIALIZERS': {},
+}
+
+# simple_jwt conf
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
+
 # DRF conf
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
